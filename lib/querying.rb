e578f4966830945fa07185d5813ed6eed25dc618
github username <github email address>
2018-05-21 21:20:59 -0400

    filled and queried tables as asked

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY length(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors LEFT JOIN series ON authors.id = series.author_id LEFT JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
+# CHECK SQLITE REPL -
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM (SELECT series.title AS title, characters.species AS species FROM characters LEFT JOIN series ON series.id = characters.series_id) GROUP BY title HAVING (species = 'human') ORDER BY COUNT(*) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) FROM characters LEFT JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(*) DESC;"
 end
